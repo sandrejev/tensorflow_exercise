71c966994d4d7812d6d1e51854f3cc15431cda66
Sergej Andrejev <sergej.andrejev@embl.de>
2017-12-31 20:23:40 +0100

    Continue fixing yolo model

--- lesson8_yolo.py
+++ lesson8_yolo.py
@@ -7,45 +7,54 @@ from os.path import basename
 import re
 import cv2
 import xml.etree.ElementTree as etree
+import matplotlib.pyplot as plt
+from utils import BBox, draw_bbox, draw_grid, draw_grid_values
 from utils import *
 
 def voc2012_get_annotation(self, path):
+    grid_shape = self.grid_shape
+    classes = self.classes
+    classes_len = len(classes)
+    target_shape = self.target_size
+    bnum = self.anchors_size
+    self.grid_shape = grid_shape
+
     img = cv2.imread(path)[..., [2, 1, 0]].copy()
-    img_scaler = tuple(s[1] / float(s[0]) for s in zip(img.size, self.target_size))
-    img = cv2.resize(img, dsize=self.target_size)
-    img = img / 255.
 
-    y = np.zeros(self.grid_shape + (self.anchors_size*5 + len(self.classes),), dtype=np.float)
-    y_grid = np.zeros(self.grid_shape, dtype=np.int) # Count
-
+    y = np.zeros(grid_shape + (bnum * 5 + classes_len,), dtype=np.float)
+    grid_nbox = np.zeros(grid_shape, dtype=np.int)  # Count
     features_path = "VOCdevkit/VOC2012/Annotations/" + re.sub(".jpg", ".xml", basename(path))
     xml = etree.parse(features_path)
     for node in xml.findall(".//object"):
-        y_class = self.classes.index(node.find("./name").text)
+        y_class = classes.index(node.find("./name").text)
 
         # Read information about bounding box and rescale it to target size
         xmax, xmin, ymax, ymin = [float(el.text) for el in sorted(node.findall("./bndbox/*"), key=lambda el: el.tag)]
-        bb = BBox.from_corners(xmin*img_scaler[1], xmax*img_scaler[1], ymin*img_scaler[0], ymax*img_scaler[0])
-        bb_cell, bb_rel = bb.grid_position(self.target_size, self.grid_shape)
+        bb = BBox.from_corners(xmin, xmax, ymin, ymax)
+        bb_cell, bb_rel = position_absolute2grid(img, grid_shape, (bb.y, bb.x))
 
         # Check whether this grid cell is already full
-        if y_grid[bb_cell] >= self.anchors_size:
+        cell_nbox = grid_nbox[bb_cell[0], bb_cell[1]]
+        if cell_nbox >= bnum:
             continue
 
-        y[bb_cell[0], bb_cell[1], (y_grid[bb_cell]*5):(y_grid[bb_cell]*5+2)] = bb_rel
-        y[bb_cell[0], bb_cell[1], (y_grid[bb_cell]*5+2):(y_grid[bb_cell]*5+4)] = [bb.w / self.target_size[0], bb.h / self.target_size[1]]
-        y[bb_cell[0], bb_cell[1], (y_grid[bb_cell]*5+4)] = 1.
-        y[bb_cell[0], bb_cell[1], 5*anchors_size + y_class] = 1
-        y_grid[bb_cell] += 1
+        y[bb_cell[0], bb_cell[1], (cell_nbox * 5):(cell_nbox * 5 + 2)] = bb_rel
+        y[bb_cell[0], bb_cell[1], (cell_nbox * 5 + 2):(cell_nbox * 5 + 4)] = [bb.h / img.shape[0], bb.w / img.shape[1]]
+        y[bb_cell[0], bb_cell[1], (cell_nbox * 5 + 4)] = 1.
+        y[bb_cell[0], bb_cell[1], 5 * bnum + y_class] = 1.
+        grid_nbox[bb_cell[0], bb_cell[1]] += 1
 
-    return img, y
+        #print(bb_cell, bb_rel, [bb.h / img.shape[0], bb.w / img.shape[1]], (bb.y, bb.x, bb.h, bb.w))
+
+    x = cv2.resize(img, dsize=target_shape) / 255.
+    return x, y
 
 
 
-epochs = 128
-batch_size = 128
+epochs = 600
+batch_size = 64
 grid_shape = (7, 7)
-anchors_size=2
+anchors_size=64
 l2_weights = 1e-4
 input_shape = (448, 448, 3)
 anchors =  [1.3221, 1.73145, 3.19275, 4.00944, 5.05587, 8.09892, 9.47112, 4.84053, 11.2364, 10.0071]
@@ -68,8 +77,8 @@ x = Conv2D((anchors_size*5) + len(classes), kernel_size=(1, 1), strides=(1, 1), 
 model = models.Model(inputs=input, outputs=x)
 
 model.compile(loss=yolo_loss,
-              optimizer=optimizers.Adam(lr=0.0001),
-              metrics=['accuracy'])
+              optimizer=optimizers.Adam(lr=1e-3),
+              metrics=[])
 
 test_items = glob("VOCdevkit/VOC2012/JPEGImages/*.jpg")
 train_items = glob("VOCdevkit/VOC2012/JPEGImages/*.jpg")
@@ -81,15 +90,17 @@ train_iterator = train_generator.flow_from_directory("VOCdevkit/VOC2012/JPEGImag
 ##############################
 # Train model
 ##############################
-tensorboard = callbacks.TensorBoard(log_dir="./K_YOLO/{}".format(datetime.today().strftime('%m-%d__%H-%M-%S')),
-    histogram_freq=0, write_graph=True)
+log_dir = "./K_YOLO/{}".format(datetime.today().strftime('%m-%d__%H-%M-%S'))
+tensorboard = callbacks.TensorBoard(log_dir=log_dir, write_graph=True, write_grads=False, write_images=False, histogram_freq=0)
 tensorboard.set_model(model)
+checkpoint = callbacks.ModelCheckpoint(log_dir + "model-{epoch:04d}.hdf5", period=50)
+reduce_lr = callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=1e-6)
 model.fit_generator(
     generator=train_iterator,
     epochs=epochs,
     validation_data=test_iterator,
     validation_steps=len(test_items) // batch_size,
-    callbacks=[tensorboard],
+    callbacks=[tensorboard, checkpoint, reduce_lr],
     steps_per_epoch=len(train_items) // batch_size,
     verbose=True)
 
--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
-# Imports for ImageWithFeaturesDataGenerator
 from glob import glob
-from PIL import Image
 import numpy as np
 from tensorflow.python.keras._impl.keras import backend as K
 import tensorflow as tf
+import cv2
 
 
 
@@ -14,10 +13,6 @@ class BBox(object):
         self.y = y
         self.w = w
         self.h = h
-        self.xmin = self.x - self.w/2.
-        self.xmax = self.x + self.w/2.
-        self.ymin = self.y - self.h/2.
-        self.ymax = self.y + self.h/2.
 
     @staticmethod
     def from_corners(xmin, xmax, ymin, ymax):
@@ -32,12 +27,34 @@ class BBox(object):
     def center(self):
         return (self.y, self.x)
 
-    def grid_position(self, img_size, grid_shape):
-        cell_shape = tuple(s[1] / float(s[0]) for s in zip(grid_shape, img_size))
-        grid_coords = tuple(sum(self.center[i] > np.linspace(-1e-10, img_size[i], num=grid_shape[i]+1))-1 for i in range(2))
-        within_grid_coords = tuple((bb - (g+0.5)*c)/c for bb,g,c in zip(self.center, grid_coords, cell_shape))
+    @property
+    def xmin(self):
+        return self.x - self.w / 2.
 
-        return grid_coords, within_grid_coords
+    @property
+    def xmax(self):
+        return self.x + self.w / 2.
+
+    @property
+    def ymin(self):
+        return self.y - self.h / 2.
+
+    @property
+    def ymax(self):
+        return self.y + self.h / 2.
+
+    def resize(self, scaler):
+        self.x *= scaler[1]
+        self.y *= scaler[0]
+        self.w *= scaler[1]
+        self.h *= scaler[0]
+    #
+    # def grid_position(self, img_size, grid_shape):
+    #     cell_shape = tuple(s[1] / float(s[0]) for s in zip(grid_shape, img_size))
+    #     grid_coords = tuple(sum(self.center[i] > np.linspace(-1e-10, img_size[i], num=grid_shape[i]+1))-1 for i in range(2))
+    #     within_grid_coords = tuple((bb - (g+0.5)*c)/c for bb,g,c in zip(self.center, grid_coords, cell_shape))
+    #
+    #     return grid_coords, within_grid_coords
 
     def as_list(self):
         return [self.x, self.y, self.w, self.h]
@@ -85,7 +102,7 @@ class YoloImageGenerator(object):
                 batch_x[i] = x
                 batch_y[i] = y
 
-            print("batch {}: {}".format(batch_index, np.sum(batch_y)))
+            #print("batch {}: {}".format(batch_index, np.sum(batch_y)))
 
             yield batch_x, batch_y
 
@@ -93,9 +110,9 @@ class YoloImageGenerator(object):
 #grid=0, batch_size=0, anchors_size=0, debug=False
 def yolo_loss(y_true, y_pred):
     anchors_size = 2 # TODO: detect automatically
-    pred_box_xy = tf.concat([y_pred[..., 0:5 * anchors_size:5], y_pred[..., 1:5 * anchors_size:5]], 3)
-    pred_box_wh = tf.concat([y_pred[..., 2:5 * anchors_size:5], y_pred[..., 3:5 * anchors_size:5]], 3)
-    pred_box_conf = y_pred[..., 4:5 * anchors_size:5]
+    pred_box_xy = tf.tanh(tf.concat([y_pred[..., 0:5 * anchors_size:5], y_pred[..., 1:5 * anchors_size:5]], 3))
+    pred_box_wh = tf.sigmoid(tf.concat([y_pred[..., 2:5 * anchors_size:5], y_pred[..., 3:5 * anchors_size:5]], 3))
+    pred_box_conf = tf.sigmoid(y_pred[..., 4:5 * anchors_size:5])
     pred_class = y_pred[..., (anchors_size * 5):]
 
     true_box_xy = tf.concat([y_true[..., 0:5 * anchors_size:5], y_true[..., 1:5 * anchors_size:5]], 3)
@@ -111,11 +128,11 @@ def yolo_loss(y_true, y_pred):
     true_cell_obj1 = tf.reduce_max(true_box_obj1, reduction_indices=[3])
 
     # Calculate parts of the loss function
-    loss_xy = tf.reduce_sum(true_box_obj2 * tf.squared_difference(pred_box_xy, true_box_xy))
-    loss_wh = tf.reduce_sum(true_box_obj2 * tf.squared_difference(tf.sqrt(pred_box_wh), tf.sqrt(true_box_wh)))
-    loss_conf = tf.reduce_sum(true_box_obj1 * tf.squared_difference(pred_box_conf, true_box_conf))
-    loss_notconf = tf.reduce_sum(true_box_noobj1 * tf.squared_difference(pred_box_conf, true_box_conf))
-    loss_class = tf.reduce_sum(true_cell_obj1 * tf.reduce_sum(tf.squared_difference(pred_class, true_class), reduction_indices=(3)))
+    loss_xy = tf.reduce_mean(tf.reduce_sum(true_box_obj2 * tf.squared_difference(pred_box_xy, true_box_xy), reduction_indices=(1,2,3)))
+    loss_wh = tf.reduce_mean(tf.reduce_sum(true_box_obj2 * tf.squared_difference(tf.sqrt(pred_box_wh), tf.sqrt(true_box_wh)), reduction_indices=(1,2,3)))
+    loss_conf = tf.reduce_mean(tf.reduce_sum(true_box_obj1 * tf.squared_difference(pred_box_conf, true_box_conf), reduction_indices=(1,2,3)))
+    loss_notconf = tf.reduce_mean(tf.reduce_sum(true_box_noobj1 * tf.squared_difference(pred_box_conf, true_box_conf), reduction_indices=(1,2,3)))
+    loss_class = tf.reduce_mean(tf.reduce_sum(true_cell_obj1 * tf.reduce_sum(tf.squared_difference(pred_class, true_class), reduction_indices=(3)), reduction_indices=(1,2)))
 
     # this is the original loss function with original coefficients from the paper
     # https://arxiv.org/pdf/1506.02640.pdf
@@ -126,3 +143,59 @@ def yolo_loss(y_true, y_pred):
     #loss = tf.Print(loss, [loss_xy, loss_wh, loss_conf, loss_notconf, loss_class], summarize=1000, message="\nloss parts:\n")
 
     return loss
+
+
+def draw_grid(img, grid_shape):
+    cell_shape = tuple(s[1] // s[0] for s in zip(grid_shape, img.shape))
+    for x in range(grid_shape[1]):
+        cv2.line(img, (cell_shape[1]*x, 0), (cell_shape[1]*x, img.shape[0]-1), (0,255,0), 1)
+    for x in range(grid_shape[0]):
+        cv2.line(img, (0, cell_shape[0]*x), (img.shape[1]-1, cell_shape[0]*x), (0,255,0), 1)
+
+
+def draw_grid_values(img, grid, offset=(0.7, 0.3), fontScale=1):
+    draw_grid(img, grid.shape)
+    cell_shape = tuple(s[1] // s[0] for s in zip(grid.shape, img.shape))
+    for (y,x), cl in np.ndenumerate(grid):
+        pt = (round((x+offset[1])*cell_shape[1]), round((y+offset[0])*cell_shape[0]))
+        cv2.putText(img, str(cl), pt, cv2.FONT_HERSHEY_DUPLEX, fontScale, color=(0,255,0), thickness=1)
+
+
+def draw_bbox(img, bbox):
+    cv2.line(img, (int(bbox.xmin), int(bbox.ymin)), (int(bbox.xmin), int(bbox.ymax)), (255,0,0), 2)
+    cv2.line(img, (int(bbox.xmax), int(bbox.ymin)), (int(bbox.xmax), int(bbox.ymax)), (255,0,0), 2)
+    cv2.line(img, (int(bbox.xmin), int(bbox.ymin)), (int(bbox.xmax), int(bbox.ymin)), (255,0,0), 2)
+    cv2.line(img, (int(bbox.xmin), int(bbox.ymax)), (int(bbox.xmax), int(bbox.ymax)), (255,0,0), 2)
+
+def position_absolute2grid(img, grid_shape, yx):
+    yx = np.array(yx)
+    cell_shape = np.array(img.shape[:2]) / np.array(grid_shape)
+    yx_rep = np.stack([np.repeat(yx[0], grid_shape[0]), np.repeat(yx[1], grid_shape[1])])
+    grid_coords = np.stack([np.linspace(-1e-10, img.shape[0], num=grid_shape[0] + 1)[:grid_shape[0]],
+                            np.linspace(-1e-10, img.shape[1], num=grid_shape[1] + 1)[:grid_shape[1]]])
+    grid_coords = np.sum(yx_rep > grid_coords, 1) - 1
+    within_grid_coords = (yx - (grid_coords + 0.5) * cell_shape) / cell_shape * 2
+
+    return grid_coords, within_grid_coords
+
+def draw_predicted_boxes(img, prediction, classes):
+    bnum = int((prediction.shape[2] - len(classes)) / 5)
+    grid_shape = np.array(prediction.shape[:2])
+    cell_shape = np.array(img.shape[:2]) / np.array(grid_shape)
+
+    prediction_yxhw = prediction[:,:,np.tile(np.arange(4), (bnum,1)) + np.reshape(np.repeat(np.arange(0, bnum*5, 5), 4), (bnum,4))]
+    prediction_conf = prediction[...,np.arange(4, bnum*5, 5)]
+    cell_shape = np.array(img.shape[:2]) / np.array(prediction_yxhw.shape[:2])
+    for gy,gx,b in np.ndindex(*prediction_yxhw.shape[:3]):
+        if prediction_conf[gy,gx,b] < 0.1:
+            continue
+
+        yxhw = prediction_yxhw[gy,gx,b,:]
+        yx = (np.array([gy, gx])+0.5)*cell_shape + yxhw[:2]*cell_shape/2
+        hw = yxhw[2:4] * img.shape[:2]
+        print(yxhw, yx, hw)
+        #xywh = xywh.astype(int)
+        bb = BBox(yx[1], yx[0], hw[1], hw[0])
+        draw_bbox(img, bb)
+
+    draw_grid(img, grid_shape)
